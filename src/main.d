77c8e929ea7e88d5683c7a0bf28662b38eb37427
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-07 14:34:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.d
+++ src/main.d
@@ -1,7 +1,11 @@
-import std.stdio;
+import std.stdio : writefln;
 import std.range : enumerate;
+
+void arg(int argc, string argv) {
+    writefln("argv[%d] = <%s>", argc, argv);
+}
 
 void main(string[] args) {
     foreach (argc, argv; args.enumerate(0))
-        writefln("argv[%d] = <%s>", argc, argv);
+        arg(argc, argv);
 }
